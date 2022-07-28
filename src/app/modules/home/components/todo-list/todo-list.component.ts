b7006e67cbcda1561ed10bdff9c8a5c8942e82db
Abraham WSL2 <abraaopessoa@yahoo.com.br>
2022-07-28 15:16:07 -0300

    array added

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -4,30 +4,41 @@ import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-
   public taskList: Array<TaskList> = [
     {
-      task: "Minha task 1",
-      checked: true
-    }, {
-      task: "Minha task 2",
-      checked: false
-    }, {
-      task: "Minha task 3",
-      checked: true
-    }, {
-      task: "Minha task 4",
-      checked: true
+      task: 'Minha task 1',
+      checked: true,
+    },
+    {
+      task: 'Minha task 2',
+      checked: false,
+    },
+    {
+      task: 'Minha task 3',
+      checked: true,
+    },
+    {
+      task: 'Minha task 4',
+      checked: true,
+    },
+  ];
+
+  public deleteItemTaskList(index: number) {
+    this.taskList.splice(index, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Are you sure?');
+    if (confirm) {
+      this.taskList = [];
     }
-  ]
+  }
   constructor() {
     // this.taskList = []
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
