673e718dd3bb14a212fa66b9ca0c6c6861bac372
Abraham WSL2 <abraaopessoa@yahoo.com.br>
2022-08-01 22:07:54 -0300

    Todo Ready

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent implements OnInit {
+  @Output() public newTaskEvent = new EventEmitter();
+  public newTaskItem: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /**
+   * sendTaskItemToList
+   */
+  public sendTaskItemToList() {
+    console.log(this.newTaskItem);
+    this.newTaskEvent.emit(this.newTaskItem);
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,7 +6,7 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
   public taskList: Array<TaskList> = [
     {
       task: 'Minha task 1',
@@ -26,6 +26,13 @@ export class TodoListComponent implements OnInit {
     },
   ];
 
+  /**
+   * addItemFromEvent
+   */
+  public addItemFromEvent(event: string) {
+    const newItem: TaskList = { task: event, checked: false };
+    this.taskList.push(newItem);
+  }
   public deleteItemTaskList(index: number) {
     this.taskList.splice(index, 1);
   }
@@ -37,8 +44,25 @@ export class TodoListComponent implements OnInit {
     }
   }
   constructor() {
-    // this.taskList = []
+    this.taskList = JSON.parse(localStorage.getItem('lista') || '[]');
+  }
+  ngDoCheck() {
+    this.taskList.sort((first, last) => {
+      const res = Number(first.checked) - Number(last.checked);
+      console.log(first);
+      console.log(last);
+      console.log(res);
+      console.log('===================================');
+
+      return res;
+    });
+    this.updateLocalStorage();
   }
 
-  ngOnInit(): void {}
+  /**
+   * updateLocalStorage
+   */
+  public updateLocalStorage() {
+    localStorage.setItem('lista', JSON.stringify(this.taskList || []));
+  }
 }
