38525a58569cee2da80d31f40bb5e7691979290c
Abraham WSL2 <abraaopessoa@yahoo.com.br>
2022-08-08 18:47:05 -0300

    Routes started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErroPageComponent } from './home/pages/erro-page/erro-page.component';
+import { SobreComponent } from './home/pages/sobre/sobre.component';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  {
+    path: '404',
+    component: ErroPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
+import { SobreComponent } from './home/pages/sobre/sobre.component';
+import { ErroPageComponent } from './home/pages/erro-page/erro-page.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SobreComponent, ErroPageComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
