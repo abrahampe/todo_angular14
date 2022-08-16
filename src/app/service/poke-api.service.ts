549b658ed0acd213777d645ae445bfb496780a8e
Abraham WSL2 <abraaopessoa@yahoo.com.br>
2022-08-15 23:48:53 -0300

    com prolbem

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -2,7 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, mergeMap, tap } from 'rxjs/operators';
-import { PokemonList, PokemonShortData } from '../interfaces/pokemon';
+import {
+  PokemonData,
+  PokemonList,
+  PokemonShortData,
+} from '../interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +14,24 @@ import { PokemonList, PokemonShortData } from '../interfaces/pokemon';
 export class PokeApiService {
   private url: string = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
   constructor(private http: HttpClient) {}
-
-  get apiListAll(): Observable<Array<PokemonShortData>> {
+const lista = [];
+  get apiListAll(): Observable<Array<PokemonData>> {
     return this.http
       .get<PokemonList>(this.url)
-      .pipe(map((res) => res?.results));
+      .pipe(map((res) => res?.results.map((rs) => {
+        return this.apiGetOne(rs.url).subscribe(
+         res =>res.status =
+       )
+      })
+
+
+
+      ));
 
     //   (res) => res );
+  }
+
+  public apiGetOne(url: string): Observable<PokemonData> {
+    return this.http.get<PokemonData>(url).pipe((res) => res);
   }
 }
